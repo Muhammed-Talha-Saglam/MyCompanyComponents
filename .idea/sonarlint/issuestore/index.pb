c7942a2315f66a4a5f039430010ce1c087c18ade
talha.saglam <1699.mTs>
2023-11-04 13:34:36 +0300

    Initial commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,11 @@
+
+A
+gradle.properties,2/a/2afbb999f001938c88fa43fc2ef52abf0f8213e4
+@
+local.properties,0/7/0712df971a99ac4d2fccb8e0fb19f377f3374cca
+C
+settings.gradle.kts,7/4/749edfcc96398253e5b3416184e95c46621da850
+O
+myuicomponents/build.gradle.kts,c/b/cbd21878c6645852e924cbeb28a87289e02adb28
+v
+Fmyuicomponents/src/main/java/com/example/myuicomponents/MyDialogBox.kt,f/3/f3ef08256c8fab5917837beaf84d615c0482da96
---
+++ app/src/main/java/com/example/mycompanycomponents/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.mycompanycomponents
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.example.myuicomponents.MyDialogBox
+
+class MainActivity : AppCompatActivity() {
+    private val TAG = "MainActivity"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val dialogFragment = MyDialogBox(
+            title = "This is Title",
+            info = "This is info",
+            buttonMode = MyDialogBox.InfoDialogButtonMode.None,
+            nameBtnCancel = "No",
+            nameBtnConfirm = "Yes",
+            btnListener = object: MyDialogBox.ButtonClickListener {
+                override fun onBtnCancelClick() {
+                    Log.d(TAG,"onBtnCancelClick")
+                }
+                override fun onBtnConfirmClick() {
+                    Log.d(TAG,"onBtnConfirmClick")
+                }
+            },
+            isCancelableOnTouchOutside = true
+        )
+
+        dialogFragment.show(supportFragmentManager,null)
+    }
+}
